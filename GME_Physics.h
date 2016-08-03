97a71afc0dbe4b729f078fbdd0465aa3fff68139
Dartrisen <calculating.scientist@gmail.com>
2016-08-03 19:55:13 +0400

    Update GME_Physics.h

--- GME_Physics.h
+++ GME_Physics.h
@@ -1,7 +1,7 @@
 //-----------------------------------------------
-// Graph_Mode_Engine version 0.2.5
+// Graph_Mode_Engine version 0.2.6
 // GME_PHYSICS
-// Part of GMEv0.2.5
+// Part of GMEv0.2.6
 // Copyright (c) 2012-2016. All rights reserved.
 //-----------------------------------------------
 
@@ -27,29 +27,28 @@ class ClassicParticle
 public:
                     ClassicParticle();
         virtual     ~ClassicParticle();
-        explicit    ClassicParticle(Vector3& r, Vector3& a);
+        explicit    ClassicParticle(Vector3& r, Vector3&v, Vector3& a);
         Vector3     position;
-        Vector3     prevPosition;
         Vector3     velocity;
         Vector3     acceleration;
         float       radius;
         float       mass;
-        void        move (const float dt);
-        bool        collisionDetect(ClassicParticle& a, ClassicParticle& b);
-        void        solver(ClassicParticle& a, ClassicParticle& b) const;
+    virtual void    move (const float dt);
+    virtual bool    collisionDetect(ClassicParticle& a, ClassicParticle& b) const;
+    virtual void    solver(ClassicParticle& a, ClassicParticle& b) const;
 };
 
 ClassicParticle::ClassicParticle()
 {
 	position = Vector3();
-	prevPosition = Vector3();
-	velocity = Vector3();
+    velocity = Vector3();
 	acceleration = Vector3();
 }
 
-ClassicParticle::ClassicParticle(Vector3& r, Vector3& a)
+ClassicParticle::ClassicParticle(Vector3& r, Vector3& v, Vector3& a)
 {
 	position = (Vector3) r;
+    velocity = (Vector3) v;
 	acceleration = (Vector3) a;
 }
 
@@ -60,28 +59,96 @@ ClassicParticle::~ClassicParticle()
 
 void ClassicParticle::move(const float dt)
 {
-  	Vector3 delta = position - prevPosition;
-  	prevPosition = position;
-  	position += delta + acceleration * dt * dt;
+    position += velocity * dt;
+    velocity += acceleration * dt;
 }
 
-bool ClassicParticle::collisionDetect(ClassicParticle& a, ClassicParticle& b)
+bool ClassicParticle::collisionDetect(ClassicParticle& a, ClassicParticle& b) const
 {
-	Vector3 r1 = a.position;
-	Vector3 r2 = b.position;
-	if((r1-r2).abs() < (a.radius + b.radius))
+	if((a.position-b.position).abs() < (a.radius + b.radius))
 		return true;
 	else return false;
 }
 
 void ClassicParticle::solver(ClassicParticle& a, ClassicParticle& b) const
 {
-	Vector3 r1 = a.position;
-	Vector3 r2 = b.position;
-	Vector3 penetrationDirection = (r2 - r1).normalize();
-	float penetrationDepth = a.radius + b.radius - (r2 - r1).abs();
-    	a.position -= penetrationDirection * penetrationDepth * 1.0f;//0.5f;
-    	b.position += penetrationDirection * penetrationDepth * 1.0f;//0.5f;
+	Vector3 penetrationDirection = (b.position-a.position).normalize();
+	float penetrationDepth = (a.radius + b.radius) - (b.position-a.position).abs();
+    a.position -= penetrationDirection * penetrationDepth * 1.0f;
+    b.position += penetrationDirection * penetrationDepth * 1.0f;
+}
+
+class PositionBasedParticle: public ClassicParticle
+{
+public:
+                    PositionBasedParticle();
+        virtual     ~PositionBasedParticle();
+        explicit    PositionBasedParticle(Vector3& r, Vector3& a);
+        Vector3     position;
+        Vector3     acceleration;
+        Vector3     delta;
+        float       radius;
+    virtual void    move(const float dt);
+    virtual bool    collisionDetect(PositionBasedParticle& a, PositionBasedParticle& b) const;
+    virtual void    solver(PositionBasedParticle& a, PositionBasedParticle& b);
+    virtual void    push(Vector3 delta);
+};
+
+PositionBasedParticle::PositionBasedParticle()
+{
+	position = Vector3();
+	delta = Vector3();
+	acceleration = Vector3();
+}
+
+PositionBasedParticle::~PositionBasedParticle()
+{
+
+}
+    
+PositionBasedParticle::PositionBasedParticle(Vector3& r, Vector3& a)
+{
+    position = (Vector3) r;
+    acceleration = (Vector3) a;
+}
+
+void PositionBasedParticle::move(const float dt)
+{
+    delta += acceleration * dt * dt;
+    position += delta;
+}
+    
+bool PositionBasedParticle::collisionDetect(PositionBasedParticle& a, PositionBasedParticle& b) const
+{
+    if((a.position-b.position).abs() < (a.radius + b.radius))
+        return true;
+    else return false;
+}
+    
+void PositionBasedParticle::solver(PositionBasedParticle& a, PositionBasedParticle& b)
+{
+        Vector3 penetrationDirection = (b.position-a.position).normalize();
+        float penetrationDepth = (a.radius + b.radius) - (b.position-a.position).abs();
+        a.push(-penetrationDirection * penetrationDepth * 0.1f);
+        b.push(penetrationDirection * penetrationDepth * 0.1f);
+    
+        Vector3 relativeVelocity = (a.delta - b.delta);
+        float bounce = 0.0f;
+        float velocity = (1.0f + bounce) * (relativeVelocity * penetrationDirection);
+    
+        if(velocity>0)
+        {
+            a.delta += penetrationDirection * velocity * 0.5f;
+            b.delta -= penetrationDirection * velocity * 0.5f;
+        }
+
+
+}
+
+void PositionBasedParticle::push(Vector3 delta)
+{
+  position += delta;
+  delta += delta;
 }
 
 }//end namespace GME_MATH
